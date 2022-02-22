33ffcc84797729eade8775aefcb9717487284792
Woratat Kumklam <woratat_kumklam@cmu.ac.th>
2022-02-22 17:44:37 +0700

    Try with resources

--- Exercise2/src/train02/Main.java
+++ Exercise2/src/train02/Main.java
@@ -7,34 +7,31 @@ public class Main {
 
     public static void main(String[] args) {
         int input;
-        Scanner scanner = new Scanner(System.in);
 
-        do
-        {
-            try
-            {
-                System.out.print("Enter number between 1 and 50 or 0 to exit: ");
-                input = scanner.nextInt();
+        try (Scanner scanner = new Scanner(System.in)) {
+            do {
+                try {
+                    System.out.print("Enter number between 1 and 50 or 0 to exit: ");
+                    input = scanner.nextInt();
 
-                if(input < 0 || input > 50){
-                    System.out.println("Please enter only number between 1 and 50 only.\n");
-                } else {
-                    if(input == 0) {
-                        break;
-                    } else if(input%2 == 0){
-                        ButterflyShape butterflyShape = new ButterflyShape(input);
-                        butterflyShape.printButterfly();
+                    if (input < 0 || input > 50) {
+                        System.out.println("Please enter only number between 1 and 50 only.\n");
                     } else {
-                        DiamondShape diamondShape = new DiamondShape(input);
-                        diamondShape.printDiamond();
+                        if (input == 0) {
+                            break;
+                        } else if (input % 2 == 0) {
+                            ButterflyShape butterflyShape = new ButterflyShape(input);
+                            butterflyShape.printButterfly();
+                        } else {
+                            DiamondShape diamondShape = new DiamondShape(input);
+                            diamondShape.printDiamond();
+                        }
                     }
+                } catch (InputMismatchException exception) {
+                    System.out.println("Integers only, please.\n");
                 }
             }
-            catch (InputMismatchException exception)
-            {
-                System.out.println("Integers only, please.\n");
-            }
+            while (true);
         }
-        while (true);
     }
 }
